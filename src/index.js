184c0c5aba376408585831ce2edd1baeccfdbb4f
nikolayzhnr <mikalayzh@gmail.com>
2021-03-15 22:13:43 +0300

    Towel Sort task done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,44 @@
 
 // You should implement your task here.
 
+  // Проверка на заполненность массива
+  function isArrayFilled(array){
+    if(array.length != 0){
+      return 1;
+    } else{
+        return 0;
+    }
+  }
+  
+  // Проверка на тип введенно атрибута (нужен массив)
+  function isArrayNorm(array){
+    if(Array.isArray(array) == 1){
+      return isArrayFilled(array);
+    } else{
+        return 0;
+    }
+  }
+
+
 module.exports = function towelSort (matrix) {
-  return [];
+  if(isArrayNorm(matrix) == true){
+
+    let unifiedArray = [];
+
+    if(matrix.length > 1){
+      for(let i=1 ; i <= (Math.floor(matrix.length/2)+1) ; i+=2 ){
+        matrix[i].reverse();
+      }
+    }
+
+    for(let i=0 ; i < matrix.length ; i++){
+      unifiedArray = unifiedArray.concat(matrix[i]);
+    }
+    
+    
+
+    return unifiedArray;
+  } else{
+    return [];
+  }
 }
